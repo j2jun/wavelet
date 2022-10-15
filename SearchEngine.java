5149cca83d85d5f8fab7dd39f1228b7aab78bfb1
Joseph Jun <54129361+j2jun@users.noreply.github.com>
2022-10-14 21:58:12 -0700

    Created SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    List<String> strList = new ArrayList<String>();
+
+    // Tracks a list of strings
+    // Support a path for adding a new string to the list
+    // A path for querying the list of string and returning a list of all strings
+    // that have a given substring.
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/")) {
+            if (url.getPath().contains("add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].contains("s")) {
+                    if (parameters[1].contains("anewstringtoadd")) {
+                        strList = new ArrayList<String>();
+                        return String.format("%s", strList);
+                    } else {
+                        strList.add(parameters[1]);
+                        return String.format("%s", strList);
+                    }
+                } else {
+                    return "404 Not Found!";
+                }
+            } else if (url.getPath().contains("search")) {
+                String[] parameters = url.getQuery().split("=");
+                List<String> found = new ArrayList<String>();
+
+                if (parameters[0].contains("s")) {
+
+                    for (String str : strList) {
+                        if (str.contains(parameters[1])) {
+                            found.add(str);
+                        }
+                    }
+                }
+                return String.format("%s", found);
+
+            } else {
+                return String.format("%s", strList);
+            }
+        }
+        return "404 Not Found!";
+    }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
